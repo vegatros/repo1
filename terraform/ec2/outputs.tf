6f535d5f33db479aef4c7589a98edaf31f97e995
cadat <vegatros@gmail.com>
2026-02-13 23:29:27 -0500

    Comment out outputs and variables for ec2

--- terraform/ec2/outputs.tf
+++ terraform/ec2/outputs.tf
@@ -1,14 +1,14 @@
-output "vpc_id" {
-  description = "VPC ID"
-  value       = aws_vpc.main.id
-}
-
-output "ec2_instance_ids" {
-  description = "EC2 instance IDs"
-  value       = aws_instance.app[*].id
-}
-
-output "ec2_private_ips" {
-  description = "EC2 private IPs"
-  value       = aws_instance.app[*].private_ip
-}
+# output "vpc_id" {
+#   description = "VPC ID"
+#   value       = aws_vpc.main.id
+# }
+# 
+# output "ec2_instance_ids" {
+#   description = "EC2 instance IDs"
+#   value       = aws_instance.app[*].id
+# }
+# 
+# output "ec2_private_ips" {
+#   description = "EC2 private IPs"
+#   value       = aws_instance.app[*].private_ip
+# }
