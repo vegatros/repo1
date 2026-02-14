e8acd7188ec5ded63dd453cba670ec2e486daa83
cadat <vegatros@gmail.com>
2026-02-14 00:28:20 -0500

    modify vpc dns

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -15,7 +15,7 @@
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
-  enable_dns_support   = false
+  enable_dns_support   = true
 
   tags = {
     Name = "${var.project_name}-vpc"
