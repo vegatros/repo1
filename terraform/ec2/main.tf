65f7ae49b108b48fdd97bf1ff8dc36ebef57af0b
cadat <vegatros@gmail.com>
2026-02-14 00:06:35 -0500

    test vpc scan

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -12,15 +12,15 @@
 # }
 # 
 # # VPC
-# resource "aws_vpc" "main" {
-#   cidr_block           = var.vpc_cidr
-#   enable_dns_hostnames = true
-#   enable_dns_support   = true
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-#   tags = {
-#     Name = "${var.project_name}-vpc"
-#   }
-# }
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
 # 
 # # Internet Gateway
 # resource "aws_internet_gateway" "main" {
