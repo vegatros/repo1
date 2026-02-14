ca33d7492a08bfdf0d3ce92aa3079dcec0f98e75
cadat <vegatros@gmail.com>
2026-02-13 19:08:35 -0500

    Add S3 backend configuration for state management

---
+++ terraform/bedrock/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-925185632967"
+    key            = "bedrock/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
