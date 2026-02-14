f07f0fc97e2abfce5ae41bf2add18a36bd4d7ed9
cadat <vegatros@gmail.com>
2026-02-13 23:56:46 -0500

    add vpc

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -11,16 +11,16 @@
 #   region = var.aws_region
 # }
 # 
-# VPC
-# resource "aws_vpc" "main" {
-#   cidr_block           = var.vpc_cidr
-#   enable_dns_hostnames = true
-#   enable_dns_support   = true
+VPC
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-#   tags = {
-#     Name = "${var.project_name}-vpc"
-#   }
-# }
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
 # 
 # # Internet Gateway
 # resource "aws_internet_gateway" "main" {
