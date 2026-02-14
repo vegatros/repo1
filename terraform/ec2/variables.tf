11c7184d3475b9f27b472b93e41c03cee4f8fc33
cadat <vegatros@gmail.com>
2026-02-13 23:39:13 -0500

    add vpc

--- terraform/ec2/variables.tf
+++ terraform/ec2/variables.tf
@@ -1,23 +1,23 @@
-# variable "aws_region" {
-#   description = "AWS region"
-#   type        = string
-#   default     = "us-east-1"
-# }
-# 
-# variable "vpc_cidr" {
-#   description = "VPC CIDR block"
-#   type        = string
-#   default     = "10.0.0.0/16"
-# }
-# 
-# variable "project_name" {
-#   description = "Project name for resource naming"
-#   type        = string
-#   default     = "myapp"
-# }
-# 
-# variable "instance_type" {
-#   description = "EC2 instance type"
-#   type        = string
-#   default     = "t2.micro"
-# }
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "project_name" {
+  description = "Project name for resource naming"
+  type        = string
+  default     = "myapp"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
