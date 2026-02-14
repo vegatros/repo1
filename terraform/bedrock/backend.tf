132d3c1b320660d62daa8ab20294110ae89e69bb
cadat <vegatros@gmail.com>
2026-02-13 23:47:16 -0500

    Replace deprecated dynamodb_table with use_lockfile

--- terraform/bedrock/backend.tf
+++ terraform/bedrock/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "terraform-state-925185632967"
     key            = "bedrock/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock"
+    use_lockfile   = true
     encrypt        = true
   }
 }
