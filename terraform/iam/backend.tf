83a488d0dbeaee2539b1f1d6a9be645b89de6337
cadat <vegatros@gmail.com>
2026-02-14 00:01:18 -0500

    Format terraform files

--- terraform/iam/backend.tf
+++ terraform/iam/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-925185632967"
-    key            = "iam/terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile   = true
-    encrypt        = true
+    bucket       = "terraform-state-925185632967"
+    key          = "iam/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
