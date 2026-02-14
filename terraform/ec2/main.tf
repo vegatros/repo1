134b45768ea1cadbcd181bbf89d811225a457a69
cadat <vegatros@gmail.com>
2026-02-14 00:23:50 -0500

    modify vpc dns

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -15,7 +15,7 @@
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
-  enable_dns_support   = true
+  enable_dns_support   = false
 
   tags = {
     Name = "${var.project_name}-vpc"
@@ -45,16 +45,16 @@ resource "aws_vpc" "main" {
 # }
 # 
 # # Private Subnets
-resource "aws_subnet" "private" {
-  count             = 2
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index + 2)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
+# resource "aws_subnet" "private" {
+#   count             = 2
+#   vpc_id            = aws_vpc.main.id
+#   cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index + 2)
+#   availability_zone = data.aws_availability_zones.available.names[count.index]
 
-  tags = {
-    Name = "${var.project_name}-private-${count.index + 1}"
-  }
-}
+#   tags = {
+#     Name = "${var.project_name}-private-${count.index + 1}"
+#   }
+# }
 # 
 # # Public Route Table
 # resource "aws_route_table" "public" {
