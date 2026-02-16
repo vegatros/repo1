b5616bc3ceaa8112ac831aa7059878f9e5f866b5
cadat <vegatros@gmail.com>
2026-02-16 14:10:54 -0500

    Fix CKV2_AWS_11: Enable VPC flow logging

--- terraform/stacks/app1/main.tf
+++ terraform/stacks/app1/main.tf
@@ -31,6 +31,62 @@ resource "aws_default_security_group" "default" {
     Name = "${var.project_name}-default-sg-restricted"
   }
 }
+
+# VPC Flow Logs
+resource "aws_flow_log" "main" {
+  vpc_id          = aws_vpc.main.id
+  traffic_type    = "ALL"
+  iam_role_arn    = aws_iam_role.flow_logs.arn
+  log_destination = aws_cloudwatch_log_group.flow_logs.arn
+
+  tags = {
+    Name = "${var.project_name}-flow-logs"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "flow_logs" {
+  name              = "/aws/vpc/${var.project_name}-flow-logs"
+  retention_in_days = 7
+
+  tags = {
+    Name = "${var.project_name}-flow-logs"
+  }
+}
+
+resource "aws_iam_role" "flow_logs" {
+  name = "${var.project_name}-flow-logs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "vpc-flow-logs.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "flow_logs" {
+  name = "${var.project_name}-flow-logs-policy"
+  role = aws_iam_role.flow_logs.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ]
+      Effect   = "Allow"
+      Resource = "*"
+    }]
+  })
+}
 # 
 # # Internet Gateway
 # resource "aws_internet_gateway" "main" {
