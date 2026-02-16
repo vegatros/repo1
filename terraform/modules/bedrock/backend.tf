e9ee573e2b1b589874a777d51be8e8003b3e36bd
cadat <vegatros@gmail.com>
2026-02-16 16:13:24 -0500

    Remove backend.tf from modules - modules inherit backend from stacks

--- terraform/modules/bedrock/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "terraform-state-925185632967"
-    key          = "bedrock/terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true
-    encrypt      = true
-  }
-}
