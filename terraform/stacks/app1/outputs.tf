0cd810a46cc013e0e4f7591ac001e51c67ff66d8
cadat <vegatros@gmail.com>
2026-02-16 14:45:08 -0500

    Refactor app1 to use VPC and EC2 modules

--- terraform/stacks/app1/outputs.tf
+++ terraform/stacks/app1/outputs.tf
@@ -1,14 +1,24 @@
-# output "vpc_id" {
-#   description = "VPC ID"
-#   value       = aws_vpc.main.id
-# }
-# 
-# output "ec2_instance_ids" {
-#   description = "EC2 instance IDs"
-#   value       = aws_instance.app[*].id
-# }
-# 
-# output "ec2_private_ips" {
-#   description = "EC2 private IPs"
-#   value       = aws_instance.app[*].private_ip
-# }
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "Public subnet IDs"
+  value       = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs"
+  value       = module.vpc.private_subnet_ids
+}
+
+output "instance_ids" {
+  description = "EC2 instance IDs"
+  value       = module.ec2.instance_ids
+}
+
+output "instance_private_ips" {
+  description = "EC2 private IPs"
+  value       = module.ec2.instance_private_ips
+}
