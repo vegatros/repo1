fb102e79b303c4845fbbd7b219114cae23aee31c
cadat <vegatros@gmail.com>
2026-02-17 11:08:39 -0500

    Use try() in Helm provider to handle missing cluster values

--- terraform/stacks/app2/helm.tf
+++ terraform/stacks/app2/helm.tf
@@ -1,11 +1,11 @@
 provider "helm" {
   kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    host                   = try(module.eks.cluster_endpoint, "")
+    cluster_ca_certificate = try(base64decode(module.eks.cluster_certificate_authority_data), "")
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.aws_region]
+      args        = ["eks", "get-token", "--cluster-name", try(module.eks.cluster_name, "dummy"), "--region", var.aws_region]
     }
   }
 }
