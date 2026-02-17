310620ebd4cce7273d7f1fed34f8513613a43e1a
cadat <vegatros@gmail.com>
2026-02-17 13:11:10 -0500

    Add HTTP port 80 to EC2 security group

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -24,6 +24,14 @@ resource "aws_security_group" "ec2" {
     description = "SSH access"
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP access"
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
