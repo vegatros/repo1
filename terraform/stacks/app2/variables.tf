521f965d013f2a9ae6721e07c1546c003640e525
cadat <vegatros@gmail.com>
2026-02-17 11:22:20 -0500

    Remove Helm variables from app2

--- terraform/stacks/app2/variables.tf
+++ terraform/stacks/app2/variables.tf
@@ -43,27 +43,3 @@ variable "max_size" {
   type        = number
   default     = 3
 }
-
-variable "helm_replica_count" {
-  description = "Number of pod replicas"
-  type        = number
-  default     = 2
-}
-
-variable "helm_image_repository" {
-  description = "Container image repository"
-  type        = string
-  default     = "nginx"
-}
-
-variable "helm_image_tag" {
-  description = "Container image tag"
-  type        = string
-  default     = "1.21"
-}
-
-variable "enable_helm_deployment" {
-  description = "Enable Helm chart deployment"
-  type        = bool
-  default     = false
-}
