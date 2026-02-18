5f300bd571152b29460af52eec2eb6c8d49ab800
cadat <vegatros@gmail.com>
2026-02-17 23:26:35 -0500

    Move app1 EC2 to public subnet with public IP

--- terraform/stacks/app1/main.tf
+++ terraform/stacks/app1/main.tf
@@ -18,7 +18,7 @@ module "ec2" {
 
   project_name   = var.project_name
   vpc_id         = module.vpc.vpc_id
-  subnet_ids     = module.vpc.private_subnet_ids
+  subnet_ids     = module.vpc.public_subnet_ids
   instance_type  = var.instance_type
   instance_count = var.instance_count
   key_name       = var.key_name
