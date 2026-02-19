60ed0407d93b9ae551a35f00cc92d6a459696a98
cadat <vegatros@gmail.com>
2026-02-18 19:52:32 -0500

    Add HTTPS port 443 to EC2 security group

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -37,6 +37,14 @@ resource "aws_security_group" "ec2" {
     description = "HTTP from internet"
   }
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTPS from internet"
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
