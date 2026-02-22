99a90f3e5189dfdbfc1911484fc59e3ecc907742
cadat <vegatros@gmail.com>
2026-02-22 01:03:12 -0500

    security: remove HTTPS port 443 from EC2 security group

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -110,20 +110,12 @@ resource "aws_security_group" "ec2" {
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = var.alb_security_group_id != "" ? [] : ["0.0.0.0/0"]
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    cidr_blocks     = var.alb_security_group_id != "" ? [] : ["0.0.0.0/0"]
     security_groups = var.alb_security_group_id != "" ? [var.alb_security_group_id] : []
-    description = "HTTP from ALB or internet"
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "HTTPS from internet"
+    description     = "HTTP from ALB or internet"
   }
 
   egress {
