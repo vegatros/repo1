cc9aa753bb72adaf8c2c609541416c8f55668947
cadat <vegatros@gmail.com>
2026-02-21 21:52:21 -0500

    refactor: move tfvars to vars directory and update workflow

--- terraform/stacks/app2/backend.tf
+++ terraform/stacks/app2/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-925185632967"
-    key            = "app2/terraform.tfstate"  # Change app2
+    key            = "stacks/app2/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-state-lock"
