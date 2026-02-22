c374ea259bfe4f2dab863b80824dd610df9378d7
cadat <vegatros@gmail.com>
2026-02-21 23:44:38 -0500

    refactor: extract user data to separate script file

--- terraform/stacks/app1/main.tf
+++ terraform/stacks/app1/main.tf
@@ -23,25 +23,7 @@ module "ec2" {
   instance_count = var.instance_count
   key_name       = var.key_name
 
-  user_data = <<-EOF
-              #!/bin/bash
-              dnf update -y
-              dnf install -y nginx git
-              
-              # Download cloudconscious HTML as home page
-              curl -o /usr/share/nginx/html/index.html \
-                https://raw.githubusercontent.com/vegatros/q/master/terraform/stacks/app1/html/cloudconscious.html
-              
-              # Download resume page
-              curl -o /usr/share/nginx/html/resume.html \
-                https://raw.githubusercontent.com/vegatros/q/master/terraform/stacks/app1/html/resume.html
-              
-              # Start and enable nginx
-              systemctl start nginx
-              systemctl enable nginx
-              
-              echo "Nginx configured with cloudconscious home page" > /var/log/nginx-setup.log
-              EOF
+  user_data = file("${path.module}/user_data.sh")
 
   tags = {
     Environment = var.environment
